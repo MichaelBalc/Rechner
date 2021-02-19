5cdb38d1a3026508b04206da7e6a68fe5a2437d4
Michael Balc <wi19b016@technikum-wien.at>
2021-02-19 15:19:18 +0100

    Abgabe

---
+++ Taschenrechner/SpecFlowFeature1.feature
@@ -0,0 +1,27 @@
+﻿Feature: SpecFlowFeature1
+	Simple calculator
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+	Scenario: Subtract two numbers
+	Given the first number is 70
+	And the second number is 50
+	When the two numbers are subtracted
+	Then the result should be 20
+
+	Scenario: Multiply two numbers
+	Given the first number is 5
+	And the second number is 6
+	When the two numbers are multiplied
+	Then the result should be 30
+
+	Scenario: Divide two numbers
+	Given the first number is 50
+	And the second number is 5
+	When the two numbers are dubtracted
+	Then the result should be 10
---
+++ Taschenrechner/SpecFlowFeature1Steps.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Taschenrechner
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        int first;
+        int second;
+        int result;
+
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            first = p0;
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            second = p0;
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            result = first + second;
+        }
+        
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            result = first - second;
+        }
+        
+        [When(@"the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            result = first * second;
+        }
+        
+        [When(@"the two numbers are dubtracted")]
+        public void WhenTheTwoNumbersAreDubtracted()
+        {
+            result = first / second;
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            int expected = p0;
+            int actual = p0;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
